3c0d1a648d58b22f8de8ba259dcb9e67e56dd47e
David Gilson <cs@david-gilson.com>
2025-02-17 11:24:03 +0100

    first commit

---
+++ src/components/Expertise.tsx
@@ -0,0 +1,91 @@
+import React from "react";
+import '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faReact, faDocker, faPython } from '@fortawesome/free-brands-svg-icons';
+import Chip from '@mui/material/Chip';
+import '../assets/styles/Expertise.scss';
+
+const labelsFirst = [
+    "React",
+    "TypeScript",
+    "JavaScript",
+    "HTML5",
+    "CSS3",
+    "SASS",
+    "Flask",
+    "Python",
+    "SQL",
+    "PostgreSQL",
+    "Postman"
+];
+
+const labelsSecond = [
+    "Git",
+    "GitHub Actions",
+    "Docker",
+    "AWS",
+    "Azure",
+    "Linux",
+    "Snowflake",
+    "Pandas",
+    "Selenium",
+];
+
+const labelsThird = [
+    "OpenAI",
+    "Groq",
+    "LangChain",
+    "Qdrant",
+    "Hugging Face",
+    "LlamaIndex",
+    "Streamlit",
+];
+
+function Expertise() {
+    return (
+    <div className="container" id="expertise">
+        <div className="skills-container">
+            <h1>Expertise</h1>
+            <div className="skills-grid">
+                <div className="skill">
+                    <FontAwesomeIcon icon={faReact} size="3x"/>
+                    <h3>Full Stack Web Development</h3>
+                    <p>I have built a diverse array of web applications from scratch using modern technologies such as React and Flask. I have a strong proficiency in the SDLC process and frontend + backend development.</p>
+                    <div className="flex-chips">
+                        <span className="chip-title">Tech stack:</span>
+                        {labelsFirst.map((label, index) => (
+                            <Chip key={index} className='chip' label={label} />
+                        ))}
+                    </div>
+                </div>
+
+                <div className="skill">
+                    <FontAwesomeIcon icon={faDocker} size="3x"/>
+                    <h3>DevOps & Automation</h3>
+                    <p>Once the application is built, I help clients set up DevOps testing, CI/CD pipelines, and deployment automation to support the successful Go-Live.</p>
+                    <div className="flex-chips">
+                        <span className="chip-title">Tech stack:</span>
+                        {labelsSecond.map((label, index) => (
+                            <Chip key={index} className='chip' label={label} />
+                        ))}
+                    </div>
+                </div>
+
+                <div className="skill">
+                    <FontAwesomeIcon icon={faPython} size="3x"/>
+                    <h3>GenAI & LLM</h3>
+                    <p>Stay relevant in the market by leveraging the latest AI models in your projects. I have professional experience building enterprise grade GenAI-enabled solutions to empower intelligent decision making.</p>
+                    <div className="flex-chips">
+                        <span className="chip-title">Tech stack:</span>
+                        {labelsThird.map((label, index) => (
+                            <Chip key={index} className='chip' label={label} />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    );
+}
+
+export default Expertise;
---
+++ src/components/Main.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import '../assets/styles/Main.scss';
+
+function Main() {
+
+  return (
+    <div className="container">
+      <div className="about-section">
+        <div className="image-wrapper">
+          <img src="https://my-aws-assets.s3.us-west-2.amazonaws.com/portfolio-img/avatar_circle.jpeg" alt="Avatar" />
+        </div>
+        <div className="content">
+          <div className="social_icons">
+            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+          </div>
+          <h1>Yuji Sato</h1>
+          <p>Full Stack Engineer</p>
+
+          <div className="mobile_social_icons">
+            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
